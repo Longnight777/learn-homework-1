a25322c949bbc006ca6be8da694c715b25ad5ace
Longnight777 <longnight777@yandex.ru>
2020-09-18 22:18:41 +0300

    correction-1

--- 3_for.py
+++ 3_for.py
@@ -1,17 +1,14 @@
 """
-
 Домашнее задание №1
-
 Цикл for: Оценки
-
 * Создать список из словарей с оценками учеников разных классов 
   школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
 
-def Average(scores): # функция нахождения среднего арифметического суммы значений списка
-        return sum(scores) / len(scores)
+def avg(scores): # функция нахождения среднего арифметического суммы значений списка
+    return sum(scores) / len(scores)
 
 def main():
     
@@ -23,18 +20,15 @@ def main():
         {'school_class': '2b', 'scores': [5,5,4,5,5]},
         {'school_class': '2c', 'scores': [4,3,4,5,3]}]
 
-    average_score_for_the_class = 0 # начальное значение переменной для класса
-    average_score_for_the_school = 0 # начальное значение переменной для школы
-
-    print()
+    avg_class_score = 0 
+    avg_school_score = 0 
 
     for dictionary in list_of_dictionaries: # в цикле идем по словарям в списке
-        average_score_for_the_class = Average(dictionary['scores']) #  на каждой итерации в словаре берем список с оценками класса по ключу "scores", кидаем его в функцию "Average", результат записываем в переменную
-        average_score_for_the_school = average_score_for_the_school + average_score_for_the_class # на каждой итерации прибавляем среднюю оценку очередного класса к имеющейся сумме
-        print(f"Средняя оценка учеников класса '{dictionary['school_class']}': {average_score_for_the_class}") # выводим результат для очередного класса
+        avg_class_score = avg(dictionary['scores']) 
+        avg_school_score = avg_school_score + avg_class_score # прибавляем среднюю оценку класса к сумме
+        print(f"Средняя оценка учеников класса '{dictionary['school_class']}': {avg_class_score}")
 
-    print(f"\nСредняя оценка учеников школы {round(average_score_for_the_school / len(list_of_dictionaries), 1)}") # выводим результат для школы целиком, как частное суммы средних оценок всех классов
-                                                                                                               # и кол-ва классов (кол-ва словарей в исходном списке)
+    print(f"\nСредняя оценка учеников школы {round(avg_school_score / len(list_of_dictionaries), 1)}")
     
 if __name__ == "__main__":
     main()
